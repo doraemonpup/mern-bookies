96f37d8c2dac4a19e0bd58fd3e55e3361a8ad35c
Chunhawit <pupz28@gmail.com>
2022-11-27 10:42:05 +0100

    add route & function to delete a book by ide

--- server/routers/books.js
+++ server/routers/books.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getBooks, addBook } from '../controllers/books.js';
+import { getBooks, addBook, deleteBook } from '../controllers/books.js';
 
 const router = express.Router();
 
 router.get('/', getBooks);
 router.post('/', addBook);
+router.delete('/:id', deleteBook);
 
 export default router;
