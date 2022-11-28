76444b8194b10d1c00a6d8c666ea9779fa1a57aa
Chunhawit <pupz28@gmail.com>
2022-11-28 12:16:12 +0100

    some style for Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import BookList from '../components/BookList';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Dashboard = () => {
-  return <BookList />;
+  return (
+    <Box sx={{ m: 4 }}>
+      <Typography variant='h3'>All Books</Typography>
+      <BookList />
+    </Box>
+  );
 };
 
 export default Dashboard;
