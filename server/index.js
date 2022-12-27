1f8b4a2fb76007ef26f2ecc9c72c63dfa5b65de7
Chunhawit <pupz28@gmail.com>
2022-12-27 04:05:59 +0100

    some code refactor

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
 import bookRouter from './routers/books.js';
 
 const app = express();
 
+// middlewares
+app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// routes
 app.use('/books', bookRouter);
 
 if (!process.env.DB_URI) {
@@ -21,13 +23,17 @@ if (!process.env.DB_URI) {
 
 const DB_URI = process.env.DB_URI;
 const PORT = process.env.APP_PORT || 4000;
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
+// connect to db
 mongoose
-  .connect(DB_URI, options)
+  .connect(DB_URI, dbOptions)
   .then(() => {
+    // listening for requests
     app.listen(PORT, () =>
-      console.log(`Server running on: http://localhost:${PORT}`)
+      console.log(
+        `Connected to DB & Server running on http://localhost:${PORT}`
+      )
     );
   })
-  .catch(error => console.log(`${error} did not connect`));
+  .catch(error => console.log(error));
